6b7989ec93f5e6d78d2a500f67a58a1176a1effe
pedrohcleal <pedroleal360@gmail.com>
2024-10-21 21:01:15 -0300

    pre-commit refactor

--- src/main.py
+++ src/main.py
@@ -11,87 +11,111 @@ from models import IndicatorDTO, PivotDTO
 
 
 driver: WebDriver = create_driver()
-pairs: list[str] = json.loads(open('pairs.json').read())
-intervals: list[str] = ['1m', '5m', '15m', '30m', '1h', '2h', '4h', '1D', '1W', '1M']
-oscillator_table = 'div:nth-child(1)> div.tableWrapper-hvDpy38G > table > tbody > tr.row-hvDpy38G > *'
-moving_average_table = 'div.container-hvDpy38G.maTable-kg4MJrFB.tableWithAction-kg4MJrFB.tabletVertical-kg4MJrFB.tabletVertical-hvDpy38G > div.tableWrapper-hvDpy38G > table > tbody > tr.row-hvDpy38G > td'
-pivot_table = 'div.container-hvDpy38G.tabletVertical-hvDpy38G > div.container-Tv7LSjUz > div.wrapper-Tv7LSjUz > div > table > tbody > tr.row-hvDpy38G > td'
+pairs: list[str] = json.loads(open("pairs.json").read())
+intervals: list[str] = ["1m", "5m", "15m", "30m", "1h", "2h", "4h", "1D", "1W", "1M"]
+oscillator_table = (
+    "div:nth-child(1)> div.tableWrapper-hvDpy38G > table > tbody > tr.row-hvDpy38G > *"
+)
+moving_average_table = "div.container-hvDpy38G.maTable-kg4MJrFB.tableWithAction-kg4MJrFB.tabletVertical-kg4MJrFB.tabletVertical-hvDpy38G > div.tableWrapper-hvDpy38G > table > tbody > tr.row-hvDpy38G > td"
+pivot_table = "div.container-hvDpy38G.tabletVertical-hvDpy38G > div.container-Tv7LSjUz > div.wrapper-Tv7LSjUz > div > table > tbody > tr.row-hvDpy38G > td"
 
 
 def fetch_pivots(driver: WebDriver, interval, pair) -> list[PivotDTO]:
-    pivot_elements: list[WebElement] = driver.find_elements(By.CSS_SELECTOR, pivot_table)
+    pivot_elements: list[WebElement] = driver.find_elements(
+        By.CSS_SELECTOR, pivot_table
+    )
     pivot_list: list[PivotDTO] = []
-    for index in range(0, len(pivot_elements), 6):        
-        pivot_list.append(PivotDTO(
-            pair=pair,
-            interval=interval,
-            register_time=datetime.now().strftime("%d/%m/%Y %H:%M"),
-            pivot=pivot_elements[index].text,
-            classic=to_float(pivot_elements[index + 1].text),
-            fibo=to_float(pivot_elements[index + 2].text),
-            camarilla=to_float(pivot_elements[index + 3].text),
-            woodie=to_float(pivot_elements[index + 4].text),
-            dm=to_float(pivot_elements[index + 5].text)
-        ))
+    for index in range(0, len(pivot_elements), 6):
+        pivot_list.append(
+            PivotDTO(
+                pair=pair,
+                interval=interval,
+                register_time=datetime.now().strftime("%d/%m/%Y %H:%M"),
+                pivot=pivot_elements[index].text,
+                classic=to_float(pivot_elements[index + 1].text),
+                fibo=to_float(pivot_elements[index + 2].text),
+                camarilla=to_float(pivot_elements[index + 3].text),
+                woodie=to_float(pivot_elements[index + 4].text),
+                dm=to_float(pivot_elements[index + 5].text),
+            )
+        )
     return pivot_list
 
+
 def fetch_moving_averages(driver: WebDriver, interval, pair) -> list[IndicatorDTO]:
-    ma_elements: list[WebElement] = driver.find_elements(By.CSS_SELECTOR, moving_average_table)
+    ma_elements: list[WebElement] = driver.find_elements(
+        By.CSS_SELECTOR, moving_average_table
+    )
     ma_list: list[IndicatorDTO] = []
     for index in range(0, len(ma_elements), 3):
-        ma_list.append(IndicatorDTO(
-            pair=pair,
-            interval=interval,
-            register_time=datetime.now().strftime("%d/%m/%Y %H:%M"),
-            name=ma_elements[index].text,
-            value=to_float(ma_elements[index + 1].text),
-            action=ma_elements[index + 2].text
-        ))
+        ma_list.append(
+            IndicatorDTO(
+                pair=pair,
+                interval=interval,
+                register_time=datetime.now().strftime("%d/%m/%Y %H:%M"),
+                name=ma_elements[index].text,
+                value=to_float(ma_elements[index + 1].text),
+                action=ma_elements[index + 2].text,
+            )
+        )
     return ma_list
 
+
 def fetch_oscillators(driver: WebDriver, interval, pair) -> list[IndicatorDTO]:
-    oscillator_elements: list[WebElement] = driver.find_elements(By.CSS_SELECTOR, oscillator_table)
+    oscillator_elements: list[WebElement] = driver.find_elements(
+        By.CSS_SELECTOR, oscillator_table
+    )
     oscillator_list: list[IndicatorDTO] = []
     for index in range(0, len(oscillator_elements), 3):
-        oscillator_list.append(IndicatorDTO(
-            pair=pair,
-            interval=interval,
-            register_time=datetime.now().strftime("%d/%m/%Y %H:%M"),
-            name=oscillator_elements[index].text,
-            value=to_float(oscillator_elements[index + 1].text),
-            action=oscillator_elements[index + 2].text
-        ))
+        oscillator_list.append(
+            IndicatorDTO(
+                pair=pair,
+                interval=interval,
+                register_time=datetime.now().strftime("%d/%m/%Y %H:%M"),
+                name=oscillator_elements[index].text,
+                value=to_float(oscillator_elements[index + 1].text),
+                action=oscillator_elements[index + 2].text,
+            )
+        )
     return oscillator_list
+
 
 def to_float(value: str) -> float | None:
     if value == "—":
         return None
-    value = value.replace('.', '').replace(',', '.').strip().replace('−', '-')
+    value = value.replace(".", "").replace(",", ".").strip().replace("−", "-")
     return float(value)
 
+
 def scrape_pair(pair: str) -> None:
-    url: str = f'https://tradingview.com/symbols/{pair}/technicals/'
+    url: str = f"https://tradingview.com/symbols/{pair}/technicals/"
     driver.get(url)
     for interval in intervals:
-        button: WebElement = WebDriverWait(driver=driver, timeout=5).until(EC.presence_of_element_located((By.ID, interval)))
+        button: WebElement = WebDriverWait(driver=driver, timeout=5).until(
+            EC.presence_of_element_located((By.ID, interval))
+        )
         button.click()
 
         oscillators: list[IndicatorDTO] = fetch_oscillators(driver, interval, pair)
-        moving_averages: list[IndicatorDTO] = fetch_moving_averages(driver,  interval, pair)
-        pivots: list[PivotDTO] = fetch_pivots(driver,  interval, pair)
-        
-        print(f'INTERVAL = {interval}, PAIR = {pair}')
-        print('Oscillators:')
+        moving_averages: list[IndicatorDTO] = fetch_moving_averages(
+            driver, interval, pair
+        )
+        pivots: list[PivotDTO] = fetch_pivots(driver, interval, pair)
+
+        print(f"INTERVAL = {interval}, PAIR = {pair}")
+        print("Oscillators:")
         pprint(oscillators)
-        print('\nMoving Averages:')
+        print("\nMoving Averages:")
         pprint(moving_averages)
-        print('\nPivots:')
+        print("\nPivots:")
         pprint(pivots)
         print()
+
 
 def main() -> None:
     for pair in pairs:
         scrape_pair(pair)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- src/models.py
+++ src/models.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 
+
 class IndicatorDTO(BaseModel):
     pair: str
     interval: str
